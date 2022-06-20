05027da326cc370538ce160a3daaf46d3979d45b
Saulius <sauliusnesukaitis@gmail.com>
2022-06-20 21:46:34 +0300

    1

--- 1/app.py
+++ 1/app.py
@@ -11,3 +11,29 @@ print(a)
 
 a = 8
 print(float(a))
+
+print("Labas \nvakaras")
+
+zodis = "Code Academy"
+print(zodis[1])
+print(zodis[-2])
+print(zodis[5:12])
+print(zodis[5:12:2])
+print(zodis[5::2])
+print(zodis[:12:2])
+print(zodis[::-1])
+print(zodis.split())
+print(zodis.upper())
+print(zodis.replace('c', 'k'))
+print(zodis.replace('Code', 'Music'))
+
+a = 5
+zodis = "Labas"
+dar_vienas = "Sitas zodis"
+print("a lygu: " + str(a) + ", zodis: " + zodis + ", dar vienas zodis - " + dar_vienas)
+print("a lygu: " + str(a) + ", žodis: " + zodis + ", dar vienas žodis – " + dar_vienas)
+print(f"a lygu {a}, žodis: {zodis}, dar vienas žodis – {dar_vienas}")
+
+a = int(input("Įveskite pirmą skaičių "))
+b = int(input("Įveskite antrą skaičių "))
+print("Jūsų skaičių suma: ", a + b)
